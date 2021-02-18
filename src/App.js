55c4d649391b6f985083c54c2cb0e99323b9a007
Chandler Roth <hey@chandlerroth.com>
2021-02-18 00:49:04 +0000

    Add initial site files

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="header">
+	 <h1>Katas <span className="badge">wip</span></h1>
+      </div>
+      <div className="subtitle">Solve problems in your language.</div>
+      <ul className="languages">
+	  <li>JavaScript</li>
+	  <li>Python</li>
+	  <li>Java</li>
+	  <li>Bash</li>
+	  <li>C#</li>
+	  <li>C++</li>
+	  <li>C</li>
+	  <li>Go</li>
+	  <li>Kotlin</li>
+	  <li>Ruby</li>
+	  <li>Swift</li>
+	  <li>R</li>
+	  <li>Rust</li>
+	  <li>Objective-C</li>
+	  <li>Dart</li>
+	  <li>Scala</li>
+	  <li>Haskell</li>
+	  <li>Solidity</li>
+	  <li>Clojure</li>
+	  <li>...and more</li>
+      </ul>
+      <a href="https://eepurl.com/hqUZgX" className="button">Get Launch Notification</a>
+    </div>
+  );
+}
+
+export default App;
